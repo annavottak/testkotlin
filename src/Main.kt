95545a0a9a7d41d79ae68ac01c23f34a88d26de2
Anna <a.yakovleva@offergate.pro>
2024-03-01 11:59:55 +0300

    initialmessage

---
+++ src/Main.kt
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    print("Введите число в метрах:")
+    val meter= readln().toDouble()
+        println ("Это ${meter/1000} км")
+}
