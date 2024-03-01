dfdc938e473b814889fee5a26434896c110098dd
Anna <a.yakovleva@offergate.pro>
2024-03-01 13:35:07 +0300

    fix error message

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     print("Введите число в метрах:")
-    val meter= readln().toDouble()
-        println ("Это ${meter/1000} км")
+    try {
+        val meter = readln().toDouble()
+        println("Это ${meter / 1000} км")
+    } catch (e: Exception) {
+        println("Неверные вводные данные")
+    }
 }
